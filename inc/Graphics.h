9b208f8b59bf86d6fe04a341eb11c7aed126cc16
SStartek <62246580+sstartek252942@users.noreply.github.com>
2022-01-30 16:51:37 +0100

    Secret Update

--- inc/Graphics.h
+++ inc/Graphics.h
@@ -3,9 +3,16 @@
 #include <SFML/Graphics.hpp>
 
 class Graphics {
-	public:
+	private:
+		sf::RenderWindow window;
 		sf::Texture textureBoard, textureBoard2, textureWhite, textureWhiteKing, textureBlack, textureBlackKing, textureMark, textureMove, textureButton1, textureButton2, textureButton3;
 		sf::Image icon;
+		sf::Font font; 
+	public:
+		Graphics(int style = 0) : window(sf::VideoMode(800, 800), "Checkers by Szymon Startek") //Utworzenie obiektu okna o wymiarach 800x800
+		{
+			LoadAll(style);
+		}
 		
 	bool LoadTextures(int style = 0)
 	{
@@ -43,5 +50,123 @@ class Graphics {
 		}
 		return true;
 	}
+	
+	bool IsWindowOpen() {
+		return window.isOpen();
+	}
+	
+	void WindowDisplay()
+	{
+		window.display();
+	}
+	
+	void PollEvent(sf::Event & event)
+	{
+		while (window.pollEvent(event)){
+			if (event.type == sf::Event::Closed) window.close();
+		}
+	}
+	
 		
+	bool LoadAll(int style = 0)
+	{
+		if (!LoadTextures(1)){
+			std::cerr << "ERROR: Texture not loaded\n";
+			return false;
+		}
+		if (!font.loadFromFile("font/arial.ttf")){ 	//Wczytywanie czcionki "arial.tff" z pliku
+			std::cerr << "ERROR: Font not found" << std::endl; 	//Błąd przy wczytywaniu pliku: Brak pliku z czcionką
+			return false;						//Zakończenie programu z powodu nie znalezienia czcionki
+		}
+		window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());	//Ustawianie wczytanego obrazu jako ikona okna
+		return true;
+	}
+		
+	int drawStart(const Board & board){
+		//************************//
+		window.clear();	//Wyczyszczenie okna "window" ze wszystkich obiektów, aby móc narysowane nowe metodą "draw"	
+		//************************//
+		sf::Sprite sprite;
+		sprite.setTexture(textureBoard2); 	
+		sprite.setPosition(sf::Vector2f(0,0)); 
+		window.draw(sprite);
+		
+		sf::Sprite sprite2;
+		sprite2.setTexture(textureButton1); 
+		sprite2.setPosition(sf::Vector2f(300,100)); 
+		window.draw(sprite2);
+		
+		sprite2.setTexture(textureButton2); 
+		sprite2.setPosition(sf::Vector2f(300,300)); 
+		window.draw(sprite2);
+
+		sprite2.setTexture(textureButton3); 
+		sprite2.setPosition(sf::Vector2f(300,500)); 
+		window.draw(sprite2);
+		
+		//************************//
+		window.display();		//Wyświetlenie w oknie "window" wszystkich obiektów, które zostały narysowane metodą "draw"
+		//************************//
+		
+		return -1;
+	}	
+	
+	void drawEnd(const Board & board){
+		//************************//
+		window.clear();	//Wyczyszczenie okna "window" ze wszystkich obiektów, aby móc narysowane nowe metodą "draw"	
+		//************************//
+		
+		if (board.isDraw()){
+			sf::Text message("Draw: 40 move rule", font);		//Tworzenie obiektu wiadomości
+			message.move(300.f, 400.f);									//Pozycja wiadomości w oknie
+			window.draw(message);	
+		} 
+		else if(board.whiteTurn){
+			sf::Text message("Black wins", font);		//Tworzenie obiektu wiadomości
+			message.move(300.f, 400.f);					//Pozycja wiadomości w oknie
+			window.draw(message);	
+		} 
+		else{
+			sf::Text message("White wins", font);		//Tworzenie obiektu wiadomości
+			message.move(300.f, 400.f);					//Pozycja wiadomości w oknie
+			window.draw(message);	
+		}
+		
+		//************************//
+		window.display();		//Wyświetlenie w oknie "window" wszystkich obiektów, które zostały narysowane metodą "draw"
+		//************************//
+	}
+	
+	void drawBoard(const Board & board, int mouseTileX1, int mouseTileY1){
+		//************************//
+		window.clear();	//Wyczyszczenie okna "window" ze wszystkich obiektów, aby móc narysowane nowe metodą "draw"	
+		//************************//
+		sf::Sprite sprite;
+		sprite.setTexture(textureBoard); 	
+		sprite.setPosition(sf::Vector2f(0,0)); 
+		window.draw(sprite);
+		
+		sf::Sprite sprite2;
+		for (int j = 7; j >= 0; j--){
+			for (int i = 0; i < 8; i++){
+				switch(board.getSafe(i,j)){
+					case white: 	sprite2.setTexture(textureWhite); 		sprite2.setPosition(sf::Vector2f(i*100,700-j*100)); window.draw(sprite2); break;
+					case whiteKing: sprite2.setTexture(textureWhiteKing); 	sprite2.setPosition(sf::Vector2f(i*100,700-j*100)); window.draw(sprite2); break;
+					case black: 	sprite2.setTexture(textureBlack); 		sprite2.setPosition(sf::Vector2f(i*100,700-j*100)); window.draw(sprite2); break;
+					case blackKing: sprite2.setTexture(textureBlackKing); 	sprite2.setPosition(sf::Vector2f(i*100,700-j*100)); window.draw(sprite2); break;
+				}
+			}
+		}
+		
+		sf::Sprite sprite3;
+		if (mouseTileX1 != -1 && mouseTileY1 != -1){
+			sprite3.setTexture(textureMark); 		
+			sprite3.setPosition(sf::Vector2f(mouseTileX1*100,700-mouseTileY1*100)); 
+			window.draw(sprite3);
+		}
+		
+		//************************//
+		window.display();		//Wyświetlenie w oknie "window" wszystkich obiektów, które zostały narysowane metodą "draw"
+		//************************//
+	}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -15,129 +15,27 @@ enum State{
 };
 
 
-//sf::Texture textureBoard, textureBoard2, textureWhite, textureWhiteKing, textureBlack, textureBlackKing, textureMark, textureMove, textureButton1, textureButton2, textureButton3;
-Graphics gfx;
-sf::Font font; 																		//Tworzenie obiektu czcionki
-sf::RenderWindow window(sf::VideoMode(800, 800), "Checkers by Szymon Startek"); 	//Utworzenie obiektu okna o wymiarach 800x800
-int mouseTileX1 = -1, mouseTileY1 = -1, mouseTileX2 = -1, mouseTileY2 = -1;
-
-int drawStart(const Board & board){
-	//************************//
-	window.clear();	//Wyczyszczenie okna "window" ze wszystkich obiektów, aby móc narysowane nowe metodą "draw"	
-	//************************//
-	sf::Sprite sprite;
-	sprite.setTexture(gfx.textureBoard2); 	
-	sprite.setPosition(sf::Vector2f(0,0)); 
-	window.draw(sprite);
-	
-	sf::Sprite sprite2;
-	sprite2.setTexture(gfx.textureButton1); 
-	sprite2.setPosition(sf::Vector2f(300,100)); 
-	window.draw(sprite2);
-	
-	sprite2.setTexture(gfx.textureButton2); 
-	sprite2.setPosition(sf::Vector2f(300,300)); 
-	window.draw(sprite2);
-
-	sprite2.setTexture(gfx.textureButton3); 
-	sprite2.setPosition(sf::Vector2f(300,500)); 
-	window.draw(sprite2);
-	
-	//************************//
-	window.display();		//Wyświetlenie w oknie "window" wszystkich obiektów, które zostały narysowane metodą "draw"
-	//************************//
-	
-	return -1;
-}
-
-void drawEnd(const Board & board){
-	//************************//
-	window.clear();	//Wyczyszczenie okna "window" ze wszystkich obiektów, aby móc narysowane nowe metodą "draw"	
-	//************************//
-	
-	if (board.isDraw()){
-		sf::Text message("Draw: 40 move rule", font);		//Tworzenie obiektu wiadomości
-		message.move(300.f, 400.f);									//Pozycja wiadomości w oknie
-		window.draw(message);	
-	} 
-	else if(board.whiteTurn){
-		sf::Text message("Black wins", font);		//Tworzenie obiektu wiadomości
-		message.move(300.f, 400.f);					//Pozycja wiadomości w oknie
-		window.draw(message);	
-	} 
-	else{
-		sf::Text message("White wins", font);		//Tworzenie obiektu wiadomości
-		message.move(300.f, 400.f);					//Pozycja wiadomości w oknie
-		window.draw(message);	
-	}
-	
-	//************************//
-	window.display();		//Wyświetlenie w oknie "window" wszystkich obiektów, które zostały narysowane metodą "draw"
-	//************************//
-}
-
-void drawBoard(const Board & board){
-	//************************//
-	window.clear();	//Wyczyszczenie okna "window" ze wszystkich obiektów, aby móc narysowane nowe metodą "draw"	
-	//************************//
-	sf::Sprite sprite;
-	sprite.setTexture(gfx.textureBoard); 	
-	sprite.setPosition(sf::Vector2f(0,0)); 
-	window.draw(sprite);
-	
-	sf::Sprite sprite2;
-	for (int j = 7; j >= 0; j--){
-		for (int i = 0; i < 8; i++){
-			switch(board.getSafe(i,j)){
-				case white: 	sprite2.setTexture(gfx.textureWhite); 		sprite2.setPosition(sf::Vector2f(i*100,700-j*100)); window.draw(sprite2); break;
-				case whiteKing: sprite2.setTexture(gfx.textureWhiteKing); 	sprite2.setPosition(sf::Vector2f(i*100,700-j*100)); window.draw(sprite2); break;
-				case black: 	sprite2.setTexture(gfx.textureBlack); 		sprite2.setPosition(sf::Vector2f(i*100,700-j*100)); window.draw(sprite2); break;
-				case blackKing: sprite2.setTexture(gfx.textureBlackKing); 	sprite2.setPosition(sf::Vector2f(i*100,700-j*100)); window.draw(sprite2); break;
-			}
-		}
-	}
-	
-	sf::Sprite sprite3;
-	if (mouseTileX1 != -1 && mouseTileY1 != -1){
-		sprite3.setTexture(gfx.textureMark); 		
-		sprite3.setPosition(sf::Vector2f(mouseTileX1*100,700-mouseTileY1*100)); 
-		window.draw(sprite3);
-	}
-	
-	//************************//
-	window.display();		//Wyświetlenie w oknie "window" wszystkich obiektów, które zostały narysowane metodą "draw"
-	//************************//
-}
-
 int main()
 {
     Board board;
     int depthAI = -1;
     State state = start;
     StarTech * ptrAI;
+    bool aiWhite = false;
+    Graphics gfx;																		//Tworzenie obiektu grafik
+    int mouseTileX1 = -1, mouseTileY1 = -1, mouseTileX2 = -1, mouseTileY2 = -1;
 	
-	if (!gfx.LoadTextures(1)){
-		std::cout << "ERROR: Texture not loaded\n";
-		return -1;
-	}
-	window.setIcon(gfx.icon.getSize().x, gfx.icon.getSize().y, gfx.icon.getPixelsPtr());	//Ustawianie wczytanego obrazu jako ikona okna
-	
-	if (!font.loadFromFile("font/arial.ttf")){ 	//Wczytywanie czcionki "arial.tff" z pliku
-		std::cerr << "ERROR: Font not found" << std::endl; 	//Błąd przy wczytywaniu pliku: Brak pliku z czcionką
-	    return -1;						//Zakończenie programu z powodu nie znalezienia czcionki
-	}
+	bool players[2] = { true, false };
 	
 	bool flagCanChangePiece = true;
 	bool flagCapture = false;
-	while (window.isOpen()){ //pętla trwająca dopóki jest otwarte okno
+	while (gfx.IsWindowOpen()){ //pętla trwająca dopóki jest otwarte okno
 		//***Sprawdzanie czy została otrzymana proźba o zamknięcie okna (np. wciśnięcie X na pasku okna)***//
 	    sf::Event event;
-	    while (window.pollEvent(event)){
-			if (event.type == sf::Event::Closed) window.close();
-		}
+		gfx.PollEvent(event);
 		
 		if (state == start){ //Wybor poziomu trudnosci
-			drawStart(board); //funkcja rysujaca plansze
+			gfx.drawStart(board); //funkcja rysujaca plansze
 			
 			if (event.type == sf::Event::MouseButtonPressed){
 				if (event.mouseButton.button == sf::Mouse::Left){
@@ -151,15 +49,42 @@ int main()
 					}
 				}
 			}
+			
+			if (true){
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
+					bool players[2] = { true, false };
+					std::cout<<"White Player vs Black StarTech\n";
+					sf::sleep(sf::milliseconds(1000));
+					aiWhite = false;
+				} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::X)) {
+					bool players[2] = { false, true };
+					std::cout<<"White StarTech vs Black Player\n";
+					sf::sleep(sf::milliseconds(1000));
+					aiWhite = true;
+				} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::C)) {
+					bool players[2] = { true, true };
+					std::cout<<"White Player vs Black Player\n";
+					sf::sleep(sf::milliseconds(1000));
+				} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::V)) {
+					bool players[2] = { true, true };
+					std::cout<<"White StarTech vs Black StarTech\n";
+					sf::sleep(sf::milliseconds(1000));
+				} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::P)) {
+					depthAI = 9;
+					std::cout<<"YOU WILL BE TERMINATED!\n";
+					sf::sleep(sf::milliseconds(1000));
+				}
+			}
+			
 						
 			if (depthAI != -1){ 
 				state = play;
-				ptrAI = new StarTech(depthAI);
+				ptrAI = new StarTech(depthAI, aiWhite);
 			}
  		}
 		else if(state == play){ //Gra
 			//****************************GRACZ********************************************//
-			if (board.whiteTurn){
+			if ((board.whiteTurn && players[0]) || (!board.whiteTurn && players[1])){
 				//Mysz
 				if (event.type == sf::Event::MouseButtonPressed){
 					if (event.mouseButton.button == sf::Mouse::Left){
@@ -221,21 +146,17 @@ int main()
 					mouseTileX1 = -1;
 					mouseTileY1 = -1;
 				}
-				/*if (!board.end()){
-					window.display();
-					board = ai2.play(board);
-				}*/
 			} 
 			else{
 			//****************************SI********************************************//
 				if (!board.end()){
-					window.display();
+					gfx.WindowDisplay();
 					board = ptrAI->play(board);
 				}
 			}
 			
 			//************************************************************************//
-			drawBoard(board); //funkcja rysujaca plansze
+			gfx.drawBoard(board,mouseTileX1, mouseTileY1); //funkcja rysujaca plansze
 			//************************************************************************//
 			
 			if (board.end()) //Koniec gry
@@ -245,7 +166,7 @@ int main()
 			}
 		}
 		else{
-			drawEnd(board);
+			gfx.drawEnd(board);
 		}
 	}
 	delete ptrAI;
